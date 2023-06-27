79115bbf6c2ff6652c05f0457f181cc477ba0a16
Adrien Tousnakhoff <105988452+Tousnaaa@users.noreply.github.com>
2023-06-27 17:55:08 +0200

    Update activation.h

--- source/activation.h
+++ source/activation.h
@@ -1,113 +1,264 @@
 #pragma once
 #include <CL/sycl.hpp>
-#include <cmath>
+
 using namespace sycl;
 using namespace sycl::ext::oneapi::experimental::matrix;
 enum class Activation {
-    ReLU,
-    LeakyReLU,
-    Exponential,
-    Sine,
-    Sigmoid,
-    Squareplus,
-    Softplus,
-    Tanh,
-    None,
+	ReLU,
+	LeakyReLU,
+	Exponential,
+	Sine,
+	Sigmoid,
+	Squareplus,
+	Softplus,
+	Tanh,
+	None,
 };
+static constexpr float PI = 3.14159265358979323846f;
 
-template<typename T, typename joint_matrix_t>
-void matrix_activation(ext::oneapi::sub_group sg, Activation activation, joint_matrix_t& frag) {
-    auto data = get_wi_data(sg, frag);
-    switch (activation) {
-    case Activation::ReLU:
-        for (int i = 0; i < data.length(); i++) {
-            if (data[i] >= 0) {
-                data[i] = data[i];
-            }
-            else {
-                data[i] = (T)0.0f;
-            }
-        }
-        return;
-    case Activation::LeakyReLU:
-        for (int i = 0; i < data.length(); i++) {
-            if (data[i] >= 0) {
-                data[i] = (T)data[i];
-            }
-            else {
-                data[i] = (T)0.01f * (float)data[i];
-            }
-        }
-        return;
-    case Activation::Exponential:
-        for (int i = 0; i < data.length(); i++) {
-            data[i] = (T)expf((float)data[i]);
-        }
-        return;
-    case Activation::Sine:
-        for (int i = 0; i < data.length(); i++) {
-            data[i] = (T)sinf((float)data[i]);
-        }
-        return;
-    case Activation::Sigmoid:
-        for (int i = 0; i < data.length(); i++) {
-            data[i] = (T)(1.0f / (1.0f + expf((float)-data[i])));
-        }
-        return;
-    case Activation::None:
-        return;
-    case Activation::Tanh:
-        for (int i = 0; i < data.length(); i++) {
-            data[i] = (T)(tanhf((float)data[i]));
-        }
-    default:
-        return;
+//template<typename T,typename joint_matrix_t>
+//void matrix_activation(ext::oneapi::sub_group& sg, Activation activation, joint_matrix_t& frag) {
+//	auto data = get_wi_data(sg, frag);
+//	switch (activation) {
+//	case Activation::ReLU:
+//		for (int i = 0; i < data.length(); i++) {
+//			if (data[i] >= 0) {
+//				data[i] = data[i];
+//			}
+//			else {
+//				data[i] = (T)0.0f;
+//			}
+//		}
+//		return;
+//	case Activation::LeakyReLU:
+//		for (int i = 0; i < data.length(); i++) {
+//			if (data[i] >= 0) {
+//				data[i] = (T)data[i];
+//			}
+//			else {
+//				data[i] = (T)0.01f*(float)data[i];
+//			}
+//		}
+//		return;
+//	case Activation::Exponential:
+//		for (int i = 0; i < data.length(); i++) {
+//			data[i] = (T)exp((float)data[i]);
+//		}
+//		return;
+//	case Activation::Sine:
+//		for (int i = 0; i < data.length(); i++) {
+//			data[i] = (T)sinf((float)data[i]);
+//		}
+//		return;
+//	case Activation::Sigmoid:
+//		for (int i = 0; i < data.length(); i++) {
+//			data[i] = (T)(1.0f / (1.0f + expf((float)-data[i])));
+//		}
+//		return;
+//	case Activation::None:
+//		return;
+//	case Activation::Tanh:
+//		for (int i = 0; i < data.length(); i++) {
+//			data[i] = (T)(tanhf((float)data[i]));
+//		}
+//	default:
+//		return;
+//
+//	}
+//}
 
-    }
+
+template<typename T>
+void elt_activation(Activation activation, T& elt) {
+	float q = ((float)elt / (2 * PI));
+	switch (activation) {
+	case Activation::ReLU:
+		if (elt < (T)0.0f) {
+			elt = (T)0.0f;
+		
+		return;
+	case Activation::LeakyReLU:
+		if (elt >= 0) {
+			elt = (T)elt;
+		}
+		else {
+			elt = (T)0.01f * (float)elt;
+		}
+		return;
+	case Activation::Exponential:
+		elt = (T)exp((float)elt);
+
+		return;
+	case Activation::Sine:
+		
+		elt = (T)((elt)-floor(q) * 2 * PI);
+		elt = (T)sinf((float)elt);
+		
+		return;
+	case Activation::Sigmoid:
+		
+		elt = (T)(1.0f / (1.0f + expf((float)-elt)));
+		return;
+	case Activation::None:
+		return;
+	case Activation::Tanh:
+		elt = (T)(tanhf((float)elt));
+	default:
+		return;
+
+		}
+	}
 }
 
 
+template<typename T>
+void matrix_activation(nd_item<1> it, Activation activation, device_ptr<T> out,int stride,stream outs) {
+	
+	int id = it.get_local_id();
+	
+	for (int i = 0; i < 8; i++) {
+		elt_activation<T>(activation, out[i * stride + id]);
+			
+	}
+	return;
+	
+}
+
+//void matrix_activation(Activation activation, float* out) {
+//	int length = 64 * 64;
+//	switch (activation) {
+//	case Activation::ReLU:
+//		for (int i = 0; i < length; i++) {
+//			if (data[i] >= 0) {
+//				data[i] = data[i];
+//			}
+//			else {
+//				data[i] = (T)0.0f;
+//			}
+//		}
+//		return;
+//	case Activation::LeakyReLU:
+//		for (int i = 0; i < length; i++) {
+//			if (data[i] >= 0) {
+//				data[i] = (T)data[i];
+//			}
+//			else {
+//				data[i] = (T)0.01f * (float)data[i];
+//			}
+//		}
+//		return;
+//	case Activation::Exponential:
+//		for (int i = 0; i < length; i++) {
+//			data[i] = (T)expf((float)data[i]);
+//		}
+//		return;
+//	case Activation::Sine:
+//		for (int i = 0; i < length; i++) {
+//			data[i] = (T)sinf((float)data[i]);
+//		}
+//		return;
+//	case Activation::Sigmoid:
+//		for (int i = 0; i < length; i++) {
+//			data[i] = (T)(1.0f / (1.0f + expf((float)-data[i])));
+//		}
+//		return;
+//	case Activation::None:
+//		return;
+//	case Activation::Tanh:
+//		for (int i = 0; i < length; i++) {
+//			data[i] = (T)(tanhf((float)data[i]));
+//		}
+//	default:
+//		return;
+//
+//	}
+//}
+//void matrix_activation_backward(Activation activation, float* out, float* forward) {
+//	switch (activation) {
+//	case Activation::ReLU:
+//		
+//			for (int t = 0; t < result.num_elements; t++) {
+//				out[t] = out[t] * (forward[t] > (T)0.0f);
+//			}
+//		return;
+//	case Activation::LeakyReLU:
+//		
+//			for (int t = 0; t < result.num_elements; t++) {
+//				out[t] = out[t] * (forward[t] > (T)0.0f ? 1.0f : 0.01f);
+//			}
+//		return;
+//	case Activation::Exponential:
+//		
+//			for (int t = 0; t < result.num_elements; t++) {
+//				out[t] = out[t] * forward[t];
+//			}
+//		return;
+//	case Activation::Sine:
+//		// Sine requires stored pre-activations, which we don't have. We only
+//		// write out the post-activations.
+//		// assert(false); // Commented out due to isolated strange side-effects on Windows
+//		return;
+//	case Activation::Sigmoid:
+//		
+//			for (int t = 0; t < result.num_elements; t++) {
+//				out[t] = out[t] * (forward[t] * (1.0f - (float)forward[t]));
+//			}
+//		return;
+//	case Activation::Tanh:
+//		
+//			for (int t = 0; t < result.num_elements; t++) {
+//				out[t] = out[t] * (1.0f - (forward[t] * forward[t]));
+//			}
+//		return;
+//	case Activation::None: result = frag; return;
+//	default:
+//		// Unsupported activation
+//		// assert(false); // Commented out due to isolated strange side-effects on Windows
+//		return;
+//	}
+//}
+
 //template<typename T,typename joint_matrix_t,typename forward_matrix_t>
 //void matrix_activation_backward(ext::oneapi::sub_group sg, Activation activation, joint_matrix_t & frag, forward_matrix_t & forward) {
-//      auto data = get_wi_data(sg, frag);
-//      auto fwd_data = get_wi_data(sg, forward);
-//      switch (activation) {
-//      case Activation::ReLU:
-//              for (int i = 0; i < data.length(); i++) {
-//                      data[i] = data[i] * relu(fwd_data[i]);
-//              }
-//              return;
-//      case Activation::LeakyReLU:
-//              for (int i = 0; i < data.length(); i++) {
-//                      if (fwd_data[i] > 0) {
-//                              data[i] = data[i];
-//                      }
-//                      else {
-//                              data[i] = data[i] * (T)0.01f;
-//                      }
-//              }
-//              return;
-//      case Activation::Exponential:
-//              for (int i = 0; i < data.length(); i++) {
-//                      data[i] = data[i] * fwd_data[i];
-//              }
-//              return;
-//      case Activation::Sine:
+//	auto data = get_wi_data(sg, frag);
+//	auto fwd_data = get_wi_data(sg, forward);
+//	switch (activation) {
+//	case Activation::ReLU:
+//		for (int i = 0; i < data.length(); i++) {
+//			data[i] = data[i] * relu(fwd_data[i]);
+//		}
+//		return;
+//	case Activation::LeakyReLU:
+//		for (int i = 0; i < data.length(); i++) {
+//			if (fwd_data[i] > 0) {
+//				data[i] = data[i];
+//			}
+//			else {
+//				data[i] = data[i] * (T)0.01f;
+//			}
+//		}
+//		return;
+//	case Activation::Exponential:
+//		for (int i = 0; i < data.length(); i++) {
+//			data[i] = data[i] * fwd_data[i];
+//		}
+//		return;
+//	case Activation::Sine:
 //
-//              return;
-//      case Activation::Sigmoid:
-//              for (int i = 0; i < data.length(); i++) {
-//                      data[i] = data[i] * (T)(fwd_data[i] * (T)(1.0f - (float)fwd_data[i]));
-//              }
-//              return;
-//      case Activation::None:
-//              return;
-//      case Activation::Tanh:
-//              for (int i = 0; i < data.length(); i++) {
-//                      data[i] = data[i] * (T)(1.0f - ((float)fwd_data[i] * (float)fwd_data[i]));
-//              }
-//      default:
-//              return;
+//		return;
+//	case Activation::Sigmoid:
+//		for (int i = 0; i < data.length(); i++) {
+//			data[i] = data[i] * (T)(fwd_data[i] * (T)(1.0f - (float)fwd_data[i]));
+//		}
+//		return;
+//	case Activation::None:
+//		return;
+//	case Activation::Tanh:
+//		for (int i = 0; i < data.length(); i++) {
+//			data[i] = data[i] * (T)(1.0f - ((float)fwd_data[i] * (float)fwd_data[i]));
+//		}
+//	default:
+//		return;
 //
-//      }
+//	}
 //}
