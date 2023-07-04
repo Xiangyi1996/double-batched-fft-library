a7a55c9fd9d5e0e912fcd30964bb09052a7d0a2b
Darius Dabert <darius.dabert@free.fr>
2023-07-04 16:41:53 +0200

    Update optimizers

--- include/Optimizers/optimizer.h
+++ include/Optimizers/optimizer.h
@@ -9,16 +9,9 @@ template <typename T>
 class Optimizer {
 public:
 	virtual ~Optimizer() {}
-	virtual void update_hyperparams(const json& params) = 0;
-	virtual json hyperparams() const = 0;
 	
 
-	virtual void step(float loss_scale, float* weights_full_precision, T* weights, const T* gradients) = 0;
-	virtual float learning_rate() const = 0;
-	virtual void set_learning_rate(float val) = 0;
-	virtual uint32_t step() const = 0;
-	virtual uint32_t n_weights() const = 0;
-	virtual T* custom_weights() const = 0;
+	virtual void step(float loss_scale, std::vector<bf16>& weights, std::vector<T>& gradients) const  = 0;
 };
 
 
--- include/Optimizers/sgd.h
+++ include/Optimizers/sgd.h
@@ -1,28 +1,25 @@
 #pragma once
-#include <optimizer.h>
+#include "optimizer.h"
 #include <vector>
 
 template<typename T>
-void sgd_step(nd_item<1> item,
-	const uint32_t n_elements,
+void sgd_step(id<1> idx,
+	const int n_elements,
 	const float loss_scale,
 	const float learning_rate,
 	const float l2_reg,
-	float* __restrict__ weights_full_precision,
-	T* __restrict__ weights,
-	const T* __restrict__ gradients
+	bf16* weights,
+	T* gradients
 ) {
-	int id = item.get_global_id();
-	if (id >= n_elements) return;
-	const float weight_fp = weights_full_precision[i];
-	float gradient = (float)gradients[i] / loss_scale;
 
-	gradient += l2_reg * weight_fp;
+	const bf16 weight = weights[idx];
+	float gradient = gradients[idx] / loss_scale;
 
-	const float new_weight = weight_fp - learning_rate * gradient;
+	gradient += l2_reg * weight;
 
-	weights_full_precision[i] = new_weight;
-	weights[i] = (T)new_weight;
+	const bf16 new_weight = weight - learning_rate * gradient;
+
+	weights[idx] = new_weight;
 }
 
 
@@ -30,38 +27,50 @@ void sgd_step(nd_item<1> item,
 template <typename T>
 class SGDOptimizer : public Optimizer<T> {
 public:
-	SGDOptimizer(const json& params) {
-		update_hyperparams(params);
-	}
-	void step(queue& q, float loss_scale, float* weights_full_precision, T* weights, const T* gradients) override {
-		++m_current_step;
-		q.submit([&](handler& h) {
-			m_nested->step(q, loss_scale, weights_full_precision, weights, gradients);
-			h.parallel_for(nd_range<1>(n_elements, n_elements), [=]nd_item it [[intel::reqd_sub_group_size(16)]] {
-				average_step(item,m_n_weights,loss_scale,m_learning_rate,m_l2_reg,weights_full_precision,weights,gradients);
-				});
-			});
-		
-	}
-	void update_hyperparams(const json& params) override {
-		if (params.contains("learning_rate")) {
-			m_learning_rate = params["learning_rate"];
-		}
 
-		if (params.contains("l2_reg")) {
-			m_l2_reg = params["l2_reg"];
-		}
-	}
-	int step() {
-		return current_step();
+	//SGDOptimizer(float learning_rate, float l2_reg) {
+	//	m_learning_rate = learning_rate;
+	//	m_l2_reg = l2_reg;
+	//}
+
+	//SGDOptimizer() {
+	//	m_learning_rate = 1e-3f;
+	//	m_l2_reg = 1e-8f;
+	//}
+
+	void step(float loss_scale, std::vector<bf16>& weights,std::vector<T>& gradients) const  override {
+		queue q;
+
+		const int n_elements = weights.size();
+		float learning_rate = m_learning_rate;
+		float l2_reg = m_l2_reg;
+
+		bf16* weights_device = malloc_shared<bf16>(weights.size(), q);
+		T* gradients_device = malloc_shared<T>(gradients.size(), q);
+
+		q.memcpy(weights_device, weights.data(), weights.size() * sizeof(bf16));
+		q.memcpy(gradients_device, gradients.data(), gradients.size() * sizeof(T));
+		q.memcpy(gradients_device, gradients.data(), gradients.size() * sizeof(T));
+		q.memcpy(gradients_device, gradients.data(), gradients.size() * sizeof(T));
+		q.memcpy(gradients_device, gradients.data(), gradients.size() * sizeof(T));
+
+		std::cout << "grad " << gradients_device[0] << std::endl;
+		std::cout << "weights_device " << weights_device[0] << std::endl;
+
+		q.parallel_for<>(range<1>(n_elements), [=](id<1> idx) {
+			sgd_step(idx, n_elements, loss_scale, learning_rate, l2_reg, weights_device, gradients_device);
+			}).wait();
+
+		q.memcpy(weights.data(), weights_device, weights.size() * sizeof(bf16));
+		std::cout << "weights " << weights[0] << std::endl;
+
+
 	}
 private:
-	uint32_t m_n_weights;
-	uint32_t m_current_step = 0;
 
-	// Hyperparameters
-	float m_learning_rate = 1e-3f;
+
+	float m_learning_rate = 1.0f;
 	float m_l2_reg = 1e-8f;
-}
+};
 
 
