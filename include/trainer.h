5128558ac9da1485c04be35ca7b08045180b0a31
Darius Dabert <darius.dabert@free.fr>
2023-07-04 16:42:09 +0200

    update trainer

--- include/trainer.h
+++ include/trainer.h
@@ -8,23 +8,24 @@ template<typename T, int WIDTH>
 class Trainer {
 public:
 
-	Trainer(int input_width, int output_width, int n_hidden_layers, Activation activation, Activation output_activation, L2Loss<T> loss ) : m_model(input_width, output_width, n_hidden_layers, activation, output_activation) {
+	Trainer(int input_width, int output_width, int n_hidden_layers, Activation activation, Activation output_activation, L2Loss<T> loss, SGDOptimizer<T> optim) : m_model(input_width, output_width, n_hidden_layers, activation, output_activation) {
 		m_loss = loss;
-		//m_optim = optim;
+		m_optim = optim;
 	}
 
 	void training_step(const std::vector<bf16>& input, std::vector<T>& output, std::vector<float>& dL_output, std::vector<T>& target, std::vector<T>& grads, std::vector<T>& losses, const float scale) {
 		m_model.initialize_params();
 		auto forward = m_model.forward_pass(input, output);
-		std::cout << target[0] << std::endl;
 		m_loss.evaluate(WIDTH, WIDTH, scale, output, target, grads, losses);
 		//m_model.backward_pass(grads, forward);
-		//m_optim.step(scale, m_model.m_weights_matrices, grads);
+		std::cout << m_model.m_weights_matrices[0] << std::endl;
+		m_optim.step(scale, m_model.m_weights_matrices, grads);
+		std::cout << m_model.m_weights_matrices[0] << std::endl;
 
 	}
 
 private:
 	SwiftNetMLP<T, WIDTH> m_model;
 	L2Loss<T> m_loss;
-	//Optimizer m_optim;
+	SGDOptimizer<T> m_optim;
 };
