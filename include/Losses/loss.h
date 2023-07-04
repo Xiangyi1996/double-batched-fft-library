65760eba7da610b9f7f0742d9464b0a9652180b6
Darius Dabert <darius.dabert@free.fr>
2023-07-04 15:18:10 +0200

    Update loss

--- include/Losses/loss.h
+++ include/Losses/loss.h
@@ -1,9 +1,14 @@
 #pragma once
-#include <cmath.h>
+//#include <cmath.h>
 
 #define SG_SIZE 8
 #define WG_SIZE 8*SG_SIZE
 #define BATCH_CHUNK 64
+
+#define TM 8
+#define TK 16
+#define TN 8
+#define SYCL_EXT_ONEAPI_MATRIX_V 4
 
 template<typename T>
 class Loss {
@@ -13,10 +18,10 @@ public:
 		const int dims,
 		const int stride,
 		const float scale,
-		const std::vector<T> pred,
-		const std::vector<float> target,
-		std::vector<T> grads,
-		std::vector<float> values
+		std::vector<T>& pred,
+		std::vector<T>& target,
+		std::vector<T>& grads,
+		std::vector<T>& values
 		) const = 0;
 };
 
