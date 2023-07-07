5ce04a33020621cda4834285c0cc9191e018d2fa
Darius Dabert <darius.dabert@free.fr>
2023-07-07 12:02:59 +0200

    update optimizers

--- include/Optimizers/optimizer.h
+++ include/Optimizers/optimizer.h
@@ -1,11 +1,9 @@
 #pragma once
 
-
-
 #include <stdint.h>
 #include <json/json.hpp>
 using json = nlohmann::json;
-template <typename T, int WIDTH>
+
 class Optimizer {
 public:
 	virtual ~Optimizer() {}
@@ -13,6 +11,3 @@ public:
 	virtual void step(float loss_scale, std::vector<bf16>& weights, std::vector<bf16>& weightsT, std::vector<bf16>& gradients) const  = 0;
 
 };
-
-
-
--- include/Optimizers/sgd.h
+++ include/Optimizers/sgd.h
@@ -2,7 +2,6 @@
 #include "optimizer.h"
 #include <vector>
 
-template<typename T>
 void sgd_step(id<1> idx,
 	const int n_elements,
 	const float loss_scale,
@@ -11,7 +10,6 @@ void sgd_step(id<1> idx,
 	bf16* weights,
 	bf16* gradients
 ) {
-
 	const bf16 weight = weights[idx];
 	float gradient = gradients[idx] / loss_scale;
 
@@ -20,10 +18,9 @@ void sgd_step(id<1> idx,
 	const bf16 new_weight = weight - learning_rate * gradient;
 
 	weights[idx] = new_weight;
-
 }
 
-template<typename T,int WIDTH>
+template<int WIDTH>
 void sgd_stepT(id<1> idx,
 	const int n_elements,
 	const float loss_scale,
@@ -32,7 +29,6 @@ void sgd_stepT(id<1> idx,
 	bf16* weightsT,
 	bf16* gradients
 ) {
-
 	const int i = idx / WIDTH;
 	const int j = idx % WIDTH;
 
@@ -46,24 +42,11 @@ void sgd_stepT(id<1> idx,
 	const bf16 new_weightT = weightT - learning_rate * gradient;
 
 	weightsT[idx] = new_weightT;
-
 }
 
-
-
-template <typename T, int WIDTH>
-class SGDOptimizer : public Optimizer<T, WIDTH> {
+template <int WIDTH>
+class SGDOptimizer : public Optimizer {
 public:
-
-	//SGDOptimizer(float learning_rate, float l2_reg) {
-	//	m_learning_rate = learning_rate;
-	//	m_l2_reg = l2_reg;
-	//}
-
-	//SGDOptimizer() {
-	//	m_learning_rate = 1e-3f;
-	//	m_l2_reg = 1e-8f;
-	//}
 
 	void step(float loss_scale, std::vector<bf16>& weights, std::vector<bf16>& weightsT, std::vector<bf16>& gradients) const  override {
 		queue q;
@@ -81,30 +64,25 @@ public:
 		q.memcpy(gradients_device, gradients.data(), gradients.size() * sizeof(bf16));
 		q.wait();
 
-		//std::cout << "grad " << gradients_device[0] << std::endl;
-		//std::cout << "weights_device " << weights_device[0] << std::endl;
-		//std::cout << "weightsT_device " << weightsT_device[0] << std::endl;
-
 		q.parallel_for<>(range<1>(n_elements), [=](id<1> idx) {
-			sgd_step<T>(idx, n_elements, loss_scale, learning_rate, l2_reg, weights_device, gradients_device);
+			sgd_step(idx, n_elements, loss_scale, learning_rate, l2_reg, weights_device, gradients_device);
 			}).wait();
 
 		q.parallel_for<>(range<1>(n_elements), [=](id<1> idx) {
-			sgd_stepT<T, WIDTH>(idx, n_elements, loss_scale, learning_rate, l2_reg, weightsT_device, gradients_device);
+			sgd_stepT<WIDTH>(idx, n_elements, loss_scale, learning_rate, l2_reg, weightsT_device, gradients_device);
 			}).wait();
 
 		q.memcpy(weights.data(), weights_device, weights.size() * sizeof(bf16));
 		q.memcpy(weightsT.data(), weightsT_device, weightsT.size() * sizeof(bf16));
 		q.wait();
-		//std::cout << "weights " << weights[0] << std::endl;
-
-
 	}
+
+	void set_learning_rate(const float learning_rate) {
+		m_learning_rate = learning_rate;
+	}
+
 private:
 
-
-	float m_learning_rate = 1e-1f;
+	float m_learning_rate = 1e-3f;
 	float m_l2_reg = 1e-8f;
 };
-
-
