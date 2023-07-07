f8d88715469d5de0d49939a82bb478acdcde0d5f
Darius Dabert <darius.dabert@free.fr>
2023-07-07 12:02:38 +0200

    Update swiftnetmlp

--- include/Network/SwiftNetMLP.h
+++ include/Network/SwiftNetMLP.h
@@ -4,20 +4,21 @@
 
 using bf16 = sycl::ext::oneapi::bfloat16;
 
-template <typename T, int WIDTH>
+template <int WIDTH>
 class SwiftNetMLP {
 public:
 	SwiftNetMLP(int input_width, int output_width, int n_hidden_layers, Activation activation, Activation output_activation);
 
-	std::vector<bf16> forward_pass(const std::vector<bf16>& input, std::vector<T>& output);
+	std::vector<bf16> forward_pass(const std::vector<bf16>& input, std::vector<float>& output);
 
 	void backward_pass(
 		const std::vector<bf16>& input, std::vector<bf16>& grads, std::vector<bf16>& forward,std::vector<bf16>& act_fwd
 	);
 	void dgemm_last_layer_backward(std::vector<bf16>& grads, std::vector<bf16>& forward, std::vector<bf16>& act_fwd, std::vector<bf16>& loss, int batch_size);
-	void set_params(T* params, T* inference_params, T* gradients);
+	//void set_params(float* params, float* inference_params, float* gradients);
 
 	void initialize_params();
+
 	std::vector<bf16> m_grads_matrices;
 	std::vector<bf16> m_weights_matrices;
 	std::vector<bf16> m_weightsT_matrices;
@@ -35,8 +36,5 @@ private:
 
 	std::vector<bf16> m_weights_matrices_inferences;
 	
-
-
 	int m_total_n_params;
-
 }; 
