2a00f3e3b9bd13cb4ea311df322f6883c720888a
Adrien Tousnakhoff <105988452+Tousnaaa@users.noreply.github.com>
2023-07-12 13:32:31 +0200

    Update SwiftNetMLP.h

--- include/Network/SwiftNetMLP.h
+++ include/Network/SwiftNetMLP.h
@@ -8,35 +8,36 @@ using bf16 = sycl::ext::oneapi::bfloat16;
 template <int WIDTH>
 class SwiftNetMLP {
 public:
-	SwiftNetMLP(queue q, int input_width, int output_width, int n_hidden_layers, Activation activation, Activation output_activation);
+    SwiftNetMLP(queue q, int input_width, int output_width, int n_hidden_layers, Activation activation, Activation output_activation);
 
-	DeviceMem<bf16> forward_pass(const DeviceMem<bf16>& input, DeviceMem<float>& output);
+    DeviceMem<bf16> forward_pass(const DeviceMem<bf16>& input, DeviceMem<float>& output);
 
-	void backward_pass(
-		const DeviceMem<bf16>& input, DeviceMem<bf16>& grads, DeviceMem<bf16>& forward
-	);
-	void dgemm_last_layer_backward(DeviceMem<bf16>& grads, DeviceMem<bf16>& forward, DeviceMem<bf16>& loss, int batch_size);
-	//void set_params(float* params, float* inference_params, float* gradients);
+    void backward_pass(
+        const DeviceMem<bf16>& input, DeviceMem<bf16>& grads, DeviceMem<bf16>& forward
+    );
+    void dgemm_last_layer_backward(DeviceMem<bf16>& grads, DeviceMem<bf16>& forward, DeviceMem<bf16>& loss, int batch_size);
+    //void set_params(float* params, float* inference_params, float* gradients);
+    void save_to_file(std::string filename);
+    void load_from_file(std::string filename);
+    void initialize_params();
 
-	void initialize_params();
-
-	DeviceMem<bf16> m_grads_matrices;
-	DeviceMem<bf16> m_weights_matrices;
-	DeviceMem<bf16> m_weightsT_matrices;
-	queue m_q;
+    DeviceMem<bf16> m_grads_matrices;
+    DeviceMem<bf16> m_weights_matrices;
+    DeviceMem<bf16> m_weightsT_matrices;
+    queue m_q;
 
 private:
-	int m_n_hidden_layers;
-	int m_n_hidden_matrices;
-	int m_inputs_width;
-	int m_net_width;
-	int m_output_width;
-	int m_padded_output_width;
+    int m_n_hidden_layers;
+    int m_n_hidden_matrices;
+    int m_inputs_width;
+    int m_net_width;
+    int m_output_width;
+    int m_padded_output_width;
 
-	Activation m_activation;
-	Activation m_output_activation;
+    Activation m_activation;
+    Activation m_output_activation;
 
-	DeviceMem<bf16> m_weights_matrices_inferences;
+    DeviceMem<bf16> m_weights_matrices_inferences;
 
-	int m_total_n_params;
+    int m_total_n_params;
 };
