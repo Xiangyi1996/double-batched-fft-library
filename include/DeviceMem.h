d2eb3062110d318f6996c4797c6c2003658975cf
DariusDabert <115560159+DariusDabert@users.noreply.github.com>
2023-07-28 16:04:55 +0200

    Update DeviceMem.h

--- include/DeviceMem.h
+++ include/DeviceMem.h
@@ -30,8 +30,6 @@ public:
         m_data = malloc_device<T>(size, q);
     }
 
-
-
     void allocate(int size) {
         if (m_size != 0 || size <= 0) {
             return;
@@ -75,9 +73,7 @@ public:
     int size() const {
         return m_size;
     }
-    // Update for the future : use oneMKL RNG for weights intialization
 
-                    //Initialziation
     void initialize_normal(double dev, DeviceMem<T>& transposed, int input_width, int width, int output_width, int n_hidden, queue q) {
 
         std::default_random_engine gen;
@@ -112,7 +108,6 @@ public:
         q.memcpy(transposed.data(), dataT.data(), m_size * sizeof(T));
     }
 
-
     void initialize_normal(double dev, queue q) {
         std::default_random_engine gen;
         std::normal_distribution<double> distrib(0.0, dev);
@@ -121,19 +116,18 @@ public:
             data[i] = (T)distrib(gen);
         }
         q.memcpy(m_data, data.data(), m_size * sizeof(T));
-
-
     }
+      
     void initialize_uniform(double scale, DeviceMem<T>& transposed, int input_width, int width, int output_width, int n_hidden, queue q) {
 
         std::default_random_engine gen;
         std::uniform_real_distribution<double> distrib(0.0, scale);
 
-
         T rnd;
 
         std::vector<T> data(m_size);
         std::vector<T> dataT(m_size);
+
         for (int i = 0; i < input_width; i++) {
             for (int j = 0; j < width; j++) {
                 rnd = (T)distrib(gen);
@@ -141,6 +135,7 @@ public:
                 dataT[toPackedLayoutCoord(j * width + i, width, input_width)] = rnd;
             }
         }
+
         for (int k = 0; k < n_hidden; k++) {
             for (int i = 0; i < width; i++) {
                 for (int j = 0; j < width; j++) {
@@ -150,6 +145,7 @@ public:
                 }
             }
         }
+
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < output_width; j++) {
                 rnd = (T)distrib(gen);
@@ -159,34 +155,40 @@ public:
         }
         q.memcpy(m_data, data.data(), m_size * sizeof(T));
         q.memcpy(transposed.data(), dataT.data(), m_size * sizeof(T));
-
     }
 
-    /*void make_transposed(DeviceMem<T>& transposed, int input_width, int width, int output_width, int n_hidden,queue q) {
-            T x;
+    void make_transposed(DeviceMem<T>& transposed, int input_width, int width, int output_width, int n_hidden,queue q) {
+        auto p = m_data;
+        int i;
+        int j;
+        int mat_num;
+        int mat_offset;
 
-            for (int i = 0; i < input_width; i++) {
-                    for (int j = 0; j < width; j++) {
-                            x = m_data[toPackedLayoutCoord(i * width + j, input_width, width)];
-                            transposed.data()[toPackedLayoutCoord(j * width + i, width, input_width)] = x;
+            q.parallel_for<>(range<1>(input_width * width + n_hidden * width * width + width * output_width), [=](id<1> idx) {
+                
+                if (idx < input_width * width) {
+                    i = idx / input_width;
+                    j = idx % input_width;
+                    transposed.data()[toPackedLayoutCoord(j * width + i, width, input_width)] = m_data[toPackedLayoutCoord(i * width + j, input_width, width)];
+                }
 
-                    }
-            }
-            for (int k = 0; k < n_hidden; k++) {
-                    for (int i = 0; i < width; i++) {
-                            for (int j = 0; j < width; j++) {
-                                    x = m_data[input_width * width + k * width * width + toPackedLayoutCoord(i * width + j, width, width)];
-                                    transposed.data()[input_width * width + k * width * width + toPackedLayoutCoord(j * width + i, width, width)] = x;
-                            }
-                    }
-            }
-            for (int i = 0; i < width; i++) {
-                    for (int j = 0; j < output_width; j++) {
-                            x = m_data[input_width * width + n_hidden * width * width + toPackedLayoutCoord(i * width + j, width, output_width)];
-                            transposed.data()[input_width * width + n_hidden * width * width + toPackedLayoutCoord(j * width + i, output_width, width)] = x;
-                    }
-            }
-    }*/
+                else if (idx < input_width * width + n_hidden * width * width) {
+                    mat_num = idx / (width * width);
+                    mat_offset = (idx - input_width * width) % (width * width);
+                    i = mat_offset / input_width;
+                    j = mat_offset % input_width;
+                    transposed.data()[input_width * width + mat_num * width * width + toPackedLayoutCoord(j * width + i, width, width)] = m_data[input_width * width + mat_num * width * width + toPackedLayoutCoord(i * width + j, width, width)];
+                }
+    
+                else {
+                    mat_offset = (idx - input_width * width - n_hidden * width * width) % (width * output_width);
+                    i = mat_offset / input_width;
+                    j = mat_offset % input_width;
+                    transposed.data()[input_width * width + n_hidden * width * width + toPackedLayoutCoord(j * width + i, output_width, width)] = m_data[input_width * width + n_hidden * width * width + toPackedLayoutCoord(i * width + j, width, output_width)];
+
+                }
+            });
+    }
 
     void initialize_uniform(queue q, double scale = 1.0) {
         std::default_random_engine gen;
@@ -196,7 +198,6 @@ public:
             data[i] = (T)distrib(gen);
         }
         q.memcpy(m_data, data.data(), m_size * sizeof(T));
-
     }
 
     void initialize_xavier_unif(DeviceMem<T>& transposed, int input_width, int width, int output_width, int n_hidden, queue q) {
@@ -233,16 +234,13 @@ public:
             }).wait();
     }
 
-
     void intitialize_he_normal(DeviceMem<T>& transposed, int input_width, int width, int output_width, int n_hidden, queue q) {
         double dev = sqrt(2.0 / width);
         initialize_normal(dev, transposed, input_width, width, output_width, n_hidden, q);
     }
+
     void intitialize_he_normal(int input_width, queue q) {
         double dev = sqrt(2.0 / input_width);
         initialize_normal(dev, q);
-
     }
-
-
 };
