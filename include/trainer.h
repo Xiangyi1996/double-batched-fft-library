ae137817e6147ba691d98f88294e452abcb0ae88
Darius Dabert <darius.dabert@free.fr>
2023-07-28 15:14:24 +0200

    update trainer

--- include/trainer.h
+++ include/trainer.h
@@ -15,14 +15,58 @@ public:
 		m_optim = &optim;
 	}
 
-	void training_step(DeviceMem<bf16>& input, DeviceMem<float>& output, DeviceMem<float>& target, DeviceMem<bf16>& grads, DeviceMem<float>& losses, const float scale, const int WIDTH) {
-		const int input_size = input.size();
-		const int batch_size = std::pow(2, 19);
-		auto forward = m_network->forward_pass(input, output);
+	void training_step(DeviceMem<bf16>& input,
+		float* forward,
+		bf16* act_mem,
+		float* act_mem_temp,
+		float* A_forward,
+		float* B_forward,
+		float* C_forward, 
+		float* out_inter, 
+		float* delta_temp,
+		DeviceMem<bf16> loss,
+		float* A_backward,
+		float* B_backward,
+		float* C_backward,
+		float* A_backward_last_layer,
+		float* B_backward_last_layer,
+		float* C_backward_last_layer, 
+		float* D_backward_last_layer, 
+		float* E_backward_last_layer,
+		float* F_backward_last_layer,
+		float* A_dgemm,
+		float* B_dgemm,
+		float* C_dgemm,
+		DeviceMem<float>& output, 
+		DeviceMem<float>& target,
+		DeviceMem<bf16>& grads,
+		DeviceMem<float>& losses,
+		const float scale,
+		const int WIDTH) {
+		//const int input_size = input.size();
+		//const int batch_size = std::pow(2, 19);
+		m_network->forward_pass(input, forward, act_mem, act_mem_temp, A_forward, B_forward, C_forward, output);
 
 		m_loss->evaluate(m_network->get_queue(), WIDTH, WIDTH, scale, output, target, grads, losses);
 
-		m_network->backward_pass(input, grads, forward);
+		m_network->backward_pass(input, 
+			grads, 
+			out_inter,
+			delta_temp, 
+			loss,
+			A_backward,
+			B_backward,
+			C_backward,
+			A_backward_last_layer,
+			B_backward_last_layer,
+			C_backward_last_layer,
+			D_backward_last_layer,
+			E_backward_last_layer, 
+			F_backward_last_layer,
+			A_dgemm,
+			B_dgemm,
+			C_dgemm,
+			forward);
 
 		m_optim->step(m_network->get_queue(), scale, m_network->m_weights_matrices, m_network->m_weightsT_matrices, m_network->m_grads_matrices, WIDTH);
 
@@ -46,20 +90,19 @@ public:
 				std::cout << "weight : " << i << " : " << m_network->m_weights_matrices.data()[64 * 64 * i + 64*j] << std::endl;
 			}
 		}*/
-		/*std::vector<bf16> data = std::vector<bf16>(forward.size());
-		forward.copy_to_host(data, forward.size(), m_network->get_queue());
+		/*std::vector<float> data = std::vector<float>(std::pow(2, 17) * (WIDTH + 64 + WIDTH * 4));
+		m_network->get_queue().memcpy(data.data(), forward, std::pow(2, 17) * (WIDTH + 64 + WIDTH * 4) * sizeof(float));
 		m_network->get_queue().wait();
 
 		for (int i = 0; i < 3; i++) {
 			for (int j = 64; j < 74 ; j++) {
-				std::cout << "forward : " << i << " : " << data[64 * batch_size * i + 64*j] << std::endl;
+				std::cout << "forward : " << i << " : " << data[64 * std::pow(2, 17) * i + 64*j] << std::endl;
 			}
 		}
 		for (int j = 0; j < 10; j++) {
-			std::cout << "forward : " << 3 << " : " << data[64 * batch_size * 3 + 128 * j] << std::endl;
+			std::cout << "forward : " << 3 << " : " << data[64 * std::pow(2, 17) * 3 + 128 * j] << std::endl;
 		}*/
 
-		forward.free_mem(m_network->get_queue());
 	}
 
 	void initialize_params() {
