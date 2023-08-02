e3efcfc195a85d65a9690369baeb066e443f8597
DariusDabert <115560159+DariusDabert@users.noreply.github.com>
2023-08-02 15:20:15 +0200

    Update test_SwitNetMLP.cpp

--- source/Tests/test_SwitNetMLP.cpp
+++ source/Tests/test_SwitNetMLP.cpp
@@ -1,3 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <CL/sycl.hpp>
+#include "activation.h"
+#include "SwiftNetMLP.h"
+#include "L2.h"
+#include "sgd.h"
+#include "trainer.h"
+#include "mkl.h"
+#include "mkl_omp_offload.h"
+#include "common.h"
+#include "oneapi/mkl.hpp"
+//#include "config.h"
+
+using namespace sycl;
+using namespace sycl::ext::oneapi::experimental::matrix;
+using bf16 = sycl::ext::oneapi::bfloat16;
+
+#define TM 8
+#define TK 16
+#define TN 8
+
+#define SG_SIZE 8
+#define WG_SIZE 8*SG_SIZE
+#define BATCH_CHUNK 64
 
 template <int WIDTH, int N_ITERS, bool BACKWARD = false>
 void work_group_layer(nd_item<1> item, Activation activation, bf16* act_mem, float* act_mem_temp, bf16* weights_layer, float* out_inter, bf16* out, float* forward_act = nullptr) {
@@ -258,9 +283,9 @@ void kernel_swift_mlp(nd_item<1> item,
             nullptr);
         item.barrier();
     }
-    
+
     // Handle output layer
-        if (output_width > 16) {
+    if (output_width > 16) {
         /*work_group_layer<WIDTH, N_ITERS, false>(item,
             activation,
             act_mem + elem_idx * WIDTH,
@@ -269,7 +294,7 @@ void kernel_swift_mlp(nd_item<1> item,
             out_intermediate_layer + elem_idx * WIDTH + (n_hidden_matmuls + 1) * layer_lenght,
             nullptr);*/
 
-        //workgroup_write_output_static<WIDTH, N_ITERS>(item, act_mem, out + elem_idx * WIDTH);
+            //workgroup_write_output_static<WIDTH, N_ITERS>(item, act_mem, out + elem_idx * WIDTH);
     }
 
     else if (out) {
@@ -603,7 +628,7 @@ void SwiftNetMLP<WIDTH>::forward_pass(const DeviceMem<bf16>& input, float* forwa
 
 
         m_q.parallel_for<>(range<1>(layer_length), [=](id<1> idx) {
-            A[idx] = (float)forward[idx + (n_hidden_matrices +1)* layer_length];
+            A[idx] = (float)forward[idx + (n_hidden_matrices + 1) * layer_length];
             });
 
         m_q.parallel_for<>(range<1>(m_output_width * m_net_width), [=](id<1> idx) {
@@ -731,7 +756,7 @@ void SwiftNetMLP<WIDTH>::backward_pass(const DeviceMem<bf16>& input,
                 }).wait();
 
                 /// Backpropagation through last layer
-                /*dgemm_last_layer_backward(grads,
+                dgemm_last_layer_backward(grads,
                     forward,
                     loss,
                     batch_size,
@@ -750,9 +775,187 @@ void SwiftNetMLP<WIDTH>::backward_pass(const DeviceMem<bf16>& input,
                 case Activation::Tanh:        mlp_swiftnet_backward<WIDTH, Activation::Tanh>(m_q, m_weightsT_matrices, loss, m_grads_matrices, out_inter, delta_temp, forward, A_dgemm, B_dgemm, C_dgemm, batch_size, m_n_hidden_matrices); break;
 
                 default: throw std::runtime_error{"Unsupported activation."};
-                }*/
+                }
 
                 m_q.parallel_for<>(range<1>(s), [=](id<1> idx) {
                     p[idx] /= batch_size;
                     }).wait();
 }
+
+
+
+void test1() {
+
+    const int batch_size = std::pow(2, 10);
+    const int output_width = 128;
+    const int WIDTH = 64;
+    const int intermediate_output_size = batch_size * WIDTH * 2;
+    const int layer_length = WIDTH * batch_size;
+    const int n_hidden_matrices = 1;
+    const int net_width = 64;
+    const int inputs_width = 64;
+
+
+    const float scale = 1e-3f;
+    /*device dev = device(gpu_selector_v);
+
+    std::vector<device> subdev = {};
+
+    subdev = dev.create_sub_devices<sycl::info::partition_property::
+        partition_by_affinity_domain>(sycl::info::partition_affinity_domain::numa);
+
+    queue q = queue(subdev[0]);*/
+
+    queue q = queue();
+
+    DeviceMem<bf16> inputs = DeviceMem<bf16>(batch_size * WIDTH, q);
+    DeviceMem<float> output = DeviceMem<float>(batch_size * output_width, q);
+    DeviceMem<float> target = DeviceMem<float>(batch_size * output_width, q);
+    DeviceMem<bf16> grads = DeviceMem<bf16>(batch_size * output_width, q);
+    DeviceMem<float> losses = DeviceMem<float>(batch_size * output_width, q);
+
+
+    float* forward = malloc_device<float>(batch_size * (WIDTH + output_width + WIDTH * 4), q);
+    int shmem_size = batch_size * WIDTH * 2;
+    const size_t alignment = 4096;
+
+    auto act_mem = sycl::aligned_alloc_device<bf16>(alignment, shmem_size, q);
+    auto act_mem_temp = sycl::aligned_alloc_device<float>(alignment, shmem_size, q);
+
+    auto A_forward = sycl::aligned_alloc_device<float>(alignment, layer_length, q);
+    auto B_forward = sycl::aligned_alloc_device<float>(alignment, output_width * 64, q);
+    auto C_forward = sycl::aligned_alloc_device<float>(alignment, output_width * batch_size, q);
+
+    float* out_inter = malloc_device<float>(batch_size * WIDTH * (n_hidden_matrices + 1), q);
+    auto deltas_temp = sycl::aligned_alloc_device<float>(alignment, output_width * batch_size, q);
+    DeviceMem<bf16> deltas(output_width * batch_size, q);
+
+    auto A_backward = sycl::aligned_alloc_device<float>(alignment, WIDTH * batch_size, q);
+    auto B_backward = sycl::aligned_alloc_device<float>(alignment, batch_size * output_width, q);
+    auto C_backward = sycl::aligned_alloc_device<float>(alignment, WIDTH * output_width, q);
+
+    auto A_backward_last_layer = sycl::aligned_alloc_device<float>(alignment, grads.size(), q);
+    auto B_backward_last_layer = sycl::aligned_alloc_device<float>(alignment, output_width * WIDTH, q);
+    auto C_backward_last_layer = sycl::aligned_alloc_device<float>(alignment, WIDTH * batch_size, q);
+    auto D_backward_last_layer = sycl::aligned_alloc_device<float>(alignment, WIDTH * batch_size, q);
+    auto E_backward_last_layer = sycl::aligned_alloc_device<float>(alignment, batch_size * WIDTH, q);
+    auto F_backward_last_layer = sycl::aligned_alloc_device<float>(alignment, WIDTH * WIDTH, q);
+
+    auto A_dgemm = sycl::aligned_alloc_device<float>(alignment, batch_size * WIDTH, q);
+    auto B_dgemm = sycl::aligned_alloc_device<float>(alignment, batch_size * WIDTH, q);
+    auto C_dgemm = sycl::aligned_alloc_device<float>(alignment, WIDTH * WIDTH, q);
+
+
+    inputs.initialize_constant(bf16(2.0f), q);
+    output.initialize_constant(0.0f, q);
+    target.initialize_constant(8.0f, q);
+    grads.initialize_constant(bf16(0.0f), q);
+    losses.initialize_constant(0.0f, q);
+
+    nlohmann::json config = {
+    {"loss", {
+            {"otype", "L2"}
+    }},
+    {"optimizer", {
+            {"otype", "sgd"},
+            {"output_width", 128},
+            {"n_hidden_layers", 2},
+            {"learning_rate", 1e-3},
+            {"l2_reg", 1e-8f}
+    }},
+    {"encoding", {
+            {"otype", "HashGrid"},
+            {"n_levels", 16},
+            {"n_features_per_level", 2},
+            {"log2_hashmap_size", 19},
+            {"base_resolution", 16},
+            {"per_level_scale", 2.0},
+    }},
+    {"network", {
+            {"otype", "SwiftNetMLP"},
+            {"activation", "ReLU"},
+            {"output_activation", "None"},
+            {"n_neurons", 64},
+            {"n_hidden_layers", 2},
+    }},
+    };
+
+    //auto model = create_from_config(q, config);
+
+    L2Loss loss;
+    SGDOptimizer optim = SGDOptimizer(128, 2, 1e-3f, 1e-8f);
+    SwiftNetMLP<64> network = SwiftNetMLP<64>(q, 64, 128, 2, Activation::None, Activation::None);
+    Trainer train(network, loss, optim);
+
+    train.initialize_params();
+
+    q.parallel_for<>(range<1>(inputs.size()), [=](id<1> idx) {
+        forward[idx] = (float)inputs.data()[idx];
+        });
+
+    for (int i = 0; i < 10; i++) {
+        std::cout << i << std::endl;
+        train.training_step(inputs,
+            forward,
+            act_mem,
+            act_mem_temp,
+            A_forward,
+            B_forward,
+            C_forward,
+            out_inter,
+            deltas_temp,
+            deltas,
+            A_backward,
+            B_backward,
+            C_backward,
+            A_backward_last_layer,
+            B_backward_last_layer,
+            C_backward_last_layer,
+            D_backward_last_layer,
+            E_backward_last_layer,
+            F_backward_last_layer,
+            A_dgemm,
+            B_dgemm,
+            C_dgemm,
+            output,
+            target,
+            grads,
+            losses,
+            scale,
+            64);
+    }
+
+    inputs.free_mem(q);
+    output.free_mem(q);
+    target.free_mem(q);
+    grads.free_mem(q);
+    losses.free_mem(q);
+    free(act_mem, q);
+    free(act_mem_temp, q);
+    free(out_inter, q);
+    free(deltas_temp, q);
+    free(A_forward, q);
+    free(B_forward, q);
+    free(C_forward, q);
+    deltas.free_mem(q);
+    free(A_backward, q);
+    free(B_backward, q);
+    free(C_backward, q);
+    free(A_backward_last_layer, q);
+    free(B_backward_last_layer, q);
+    free(C_backward_last_layer, q);
+    free(D_backward_last_layer, q);
+    free(E_backward_last_layer, q);
+    free(F_backward_last_layer, q);
+    free(A_dgemm, q);
+    free(B_dgemm, q);
+    free(C_dgemm, q);
+}
+
+int main() {
+
+    test1();
+    return 0;
+}
+
+
