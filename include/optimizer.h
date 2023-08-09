4a0fe75b74cb8eced215a689eb7b2f46dba0aacd
Adrien Tousnakhoff <105988452+Tousnaaa@users.noreply.github.com>
2023-08-09 14:07:45 +0200

    Update optimizer.h

--- include/optimizer.h
+++ include/optimizer.h
@@ -11,7 +11,6 @@ class Optimizer {
 public:
 	virtual ~Optimizer() {}
 
-	virtual void step(queue q, float loss_scale, DeviceMem<bf16>& weights, DeviceMem<bf16>& weightsT, DeviceMem<bf16>& gradients, int WIDTH) const = 0;
+	virtual void step(queue q, float loss_scale, DeviceMem<bf16>& weights, DeviceMem<bf16>& weightsT, DeviceMem<bf16>& gradients, int WIDTH)= 0;
 
 };
-
