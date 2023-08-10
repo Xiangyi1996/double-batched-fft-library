5721c9a50d165cfeebebf91201e80669d2819a54
Adrien Tousnakhoff <105988452+Tousnaaa@users.noreply.github.com>
2023-08-10 11:17:10 +0200

    Update common.h

--- include/common.h
+++ include/common.h
@@ -1,14 +1,35 @@
 #pragma once
 
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 #include <CL/sycl.hpp>
 
-
-
+/**
+ * @brief Convert index from original matrix layout to packed layout
+ *
+ * @param idx Index in packed layout
+ * @param rows Number of rows in original matrix
+ * @param cols Number of columns in original matrix
+ * @return Index in packed matrix layout
+ */
 extern SYCL_EXTERNAL int toPackedLayoutCoord(int idx, int rows, int cols);
-// Row et cols correspondent au nombre de rows et cols de la matrice d'origine pas celle en packec layout ( qui est donc cols*2 rows/2)
+
+/**
+ * @brief Convert index from packed layout to original matrix layout
+ *
+ * @param idx Index in original matrix layout
+ * @param rows Number of rows in original matrix
+ * @param cols Number of columns in original matrix
+ * @return Index in original matrix layout
+ */
 extern SYCL_EXTERNAL int fromPackedLayoutCoord(int idx, int rows, int cols);
 
+/**
+ * @brief Compare two strings case-insensitively
+ *
+ * @param str1 First string
+ * @param str2 Second string
+ * @return True if the strings are equal, false otherwise
+ */
 extern SYCL_EXTERNAL bool isequalstring(const std::string& str1, const std::string& str2);
 
