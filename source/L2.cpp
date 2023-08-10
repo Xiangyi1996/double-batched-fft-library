9e773f865b533f664b30ef0a2d33e97f940aec89
Adrien Tousnakhoff <105988452+Tousnaaa@users.noreply.github.com>
2023-08-10 10:43:06 +0200

    Update L2.cpp

--- source/L2.cpp
+++ source/L2.cpp
@@ -1,6 +1,8 @@
 #include "L2.h"
 
-void L2_loss(id<1> idx,
+// Function to compute L2 loss for an element
+void L2_loss(
+    id<1> idx,
     const int n_elements,
     const int dims,
     const int stride,
@@ -8,24 +10,31 @@ void L2_loss(id<1> idx,
     float* preds,
     float* targets,
     bf16* grads,
-    float* values) {
-
+    float* values
+) {
+    // Calculate intra and inter indices
     const int intra_idx = idx % stride;
     const int inter_idx = idx / stride;
 
+    // Calculate total number of elements
     const int N_total_elements = n_elements * dims / stride;
 
+    // Calculate target index
     const int target_idx = inter_idx * dims + intra_idx;
 
+    // Compute the squared difference between preds and targets
     const float difference = (preds[idx] - targets[target_idx]);
-
     values[idx] = difference * difference;
 
+    // Compute gradient using bf16 type
     grads[idx] = bf16(((bf16)preds[idx] - (bf16)targets[target_idx]));
 }
 
-
-void L2Loss::evaluate(
+// Class for evaluating L2 loss
+class L2Loss {
+public:
+    // Evaluate L2 loss using OpenCL queue
+    void evaluate(
         queue q,
         const int dims,
         const int stride,
@@ -34,19 +43,22 @@ void L2Loss::evaluate(
         DeviceMem<float>& targets,
         DeviceMem<bf16>& grads,
         DeviceMem<float>& values
-    ){
-
+    ) {
+        // Get the total number of elements
         int n_elements = preds.size();
 
+        // Parallel computation using OpenCL
         q.parallel_for<>(range<1>(n_elements), [=](id<1> idx) {
             L2_loss(idx,
-            n_elements,
-            dims,
-            stride,
-            scale,
-            preds.data(),
-            targets.data(),
-            grads.data(),
-            values.data());
-            }).wait();
+                n_elements,
+                dims,
+                stride,
+                scale,
+                preds.data(),
+                targets.data(),
+                grads.data(),
+                values.data()
+            );
+        }).wait();
     }
+};
