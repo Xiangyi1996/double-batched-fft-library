4542c031b4114b0a1c85a4d556c6229818f01c6f
Adrien Tousnakhoff <105988452+Tousnaaa@users.noreply.github.com>
2023-08-10 16:16:03 +0200

    Update L2.cpp

--- source/L2.cpp
+++ source/L2.cpp
@@ -1,6 +1,23 @@
 #include "L2.h"
 
-// Function to compute L2 loss for an element
+/**
+ * Calculates L2 loss and gradients for each element in the input.
+ *
+ * This function computes the L2 loss and gradients between predicted values
+ * and target values for each element in the input. It calculates the squared
+ * difference between predicted and target values, and computes gradients using
+ * the bf16 type.
+ *
+ * @param idx          The current index being processed.
+ * @param n_elements   The number of elements in the batch.
+ * @param dims         The total number of dimensions per element.
+ * @param stride       The step size between consecutive elements.
+ * @param scale        A scaling factor for normalization (unused).
+ * @param preds        An array of predicted values.
+ * @param targets      An array of target values.
+ * @param grads        An array to store gradients.
+ * @param values       An array to store squared differences.
+ */
 void L2_loss(
     id<1> idx,
     const int n_elements,
@@ -13,8 +30,8 @@ void L2_loss(
     float* values
 ) {
     // Calculate intra and inter indices
-    const int intra_idx = idx % stride;
-    const int inter_idx = idx / stride;
+    const int intra_idx = idx % stride;   // Index within each element
+    const int inter_idx = idx / stride;   // Index of the element
 
     // Calculate total number of elements
     const int N_total_elements = n_elements * dims / stride;
@@ -30,31 +47,46 @@ void L2_loss(
     grads[idx] = bf16(((bf16)preds[idx] - (bf16)targets[target_idx]));
 }
 
-// Evaluate L2 loss using OpenCL queue
-void L2Loss::evaluate(
-    queue q,
-    const int dims,
-    const int stride,
-    const float scale,
-    DeviceMem<float>& preds,
-    DeviceMem<float>& targets,
-    DeviceMem<bf16>& grads,
-    DeviceMem<float>& values
-) {
-    // Get the total number of elements
-    int n_elements = preds.size();
 
-    // Parallel computation using OpenCL
-    q.parallel_for<>(range<1>(n_elements), [=](id<1> idx) {
-        L2_loss(idx,
-            n_elements,
-            dims,
-            stride,
-            scale,
-            preds.data(),
-            targets.data(),
-            grads.data(),
-            values.data()
-        );
-    }).wait();
-}
+/**
+     * Evaluates L2 loss and gradients in parallel using OpenCL.
+     *
+     * This method calculates L2 loss and gradients for each element in parallel using OpenCL.
+     * It uses the provided L2_loss function to perform the calculations for each element.
+     *
+     * @param q          The OpenCL queue for parallel execution.
+     * @param dims       The total number of dimensions per element.
+     * @param stride     The step size between consecutive elements.
+     * @param scale      A scaling factor for normalization (unused).
+     * @param preds      The predicted values for each element.
+     * @param targets    The target values for each element.
+     * @param grads      An array to store gradients.
+     * @param values     An array to store squared differences.
+     */
+    void L2Loss::evaluate(
+        queue q,
+        const int dims,
+        const int stride,
+        const float scale,
+        DeviceMem<float>& preds,
+        DeviceMem<float>& targets,
+        DeviceMem<bf16>& grads,
+        DeviceMem<float>& values
+    ) {
+        // Get the total number of elements
+        int n_elements = preds.size();
+
+        // Parallel computation using OpenCL
+        q.parallel_for<>(range<1>(n_elements), [=](id<1> idx) {
+            L2_loss(idx,
+                n_elements,
+                dims,
+                stride,
+                scale,
+                preds.data(),
+                targets.data(),
+                grads.data(),
+                values.data()
+            );
+        }).wait();
+    }
