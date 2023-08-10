7e11434409deecf5227a4980e1dbd07b6f25935a
Adrien Tousnakhoff <105988452+Tousnaaa@users.noreply.github.com>
2023-08-10 16:31:30 +0200

    Update common.cpp

--- source/common.cpp
+++ source/common.cpp
@@ -1,6 +1,19 @@
 #include "common.h"
 
-// Convert index from packed layout to original matrix layout
+
+/**
+ * Convert an index to a packed layout coordinate for interleaved even and odd rows.
+ *
+ * This function calculates the packed layout coordinate for an interleaved layout
+ * where consecutive even and odd rows are interleaved. It takes an index, the number
+ * of rows, and the number of columns as inputs and returns the corresponding packed
+ * layout coordinate.
+ *
+ * @param idx   The index to convert to a packed layout coordinate.
+ * @param rows  The number of rows in the layout.
+ * @param cols  The number of columns in the layout.
+ * @return      The packed layout coordinate for the given index.
+ */
 int toPackedLayoutCoord(int idx, int rows, int cols) {
     int i = idx / cols;
     int j = idx % cols;
@@ -12,7 +25,20 @@ int toPackedLayoutCoord(int idx, int rows, int cols) {
     }
 }
 
-// Convert index from original matrix layout to packed layout
+
+/**
+ * Convert a packed layout coordinate to an index for interleaved even and odd rows.
+ *
+ * This function calculates the original index for an interleaved layout
+ * where consecutive even and odd rows are interleaved. It takes a packed layout
+ * coordinate, the number of rows, and the number of columns as inputs and returns
+ * the corresponding index.
+ *
+ * @param idx   The packed layout coordinate to convert to an index.
+ * @param rows  The number of rows in the layout.
+ * @param cols  The number of columns in the layout.
+ * @return      The index corresponding to the given packed layout coordinate.
+ */
 int fromPackedLayoutCoord(int idx, int rows, int cols) {
     int i = idx / (cols * 2);
     int j = idx % (cols * 2);
@@ -24,7 +50,18 @@ int fromPackedLayoutCoord(int idx, int rows, int cols) {
     }
 }
 
-// Compare two strings case-insensitively
+
+/**
+ * Check if two strings are equal while ignoring the case of characters.
+ *
+ * This function compares two input strings for equality, considering the case
+ * of characters. It returns true if the strings are equal (ignoring case),
+ * and false otherwise.
+ *
+ * @param str1  The first string to compare.
+ * @param str2  The second string to compare.
+ * @return      True if the strings are equal (ignoring case), false otherwise.
+ */
 bool isequalstring(const std::string& str1, const std::string& str2) {
     if (str1.length() != str2.length()) {
         return false;
