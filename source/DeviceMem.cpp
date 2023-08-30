d120a6dc85ca3706a2bff94fcbee7130807ea371
DariusDabert <115560159+DariusDabert@users.noreply.github.com>
2023-08-30 15:04:57 +0200

    Update DeviceMem.cpp

--- source/DeviceMem.cpp
+++ source/DeviceMem.cpp
@@ -19,11 +19,11 @@ DeviceMem<T>::DeviceMem() {}
  */
 template<typename T>
 DeviceMem<T>::DeviceMem(int size, queue q) {
-    if (m_size != 0 || size <= 0) {
-        return;
-    }
-    m_size = size;
-    m_data = malloc_device<T>(size, q);
+	if (m_size != 0 || size <= 0) {
+		return;
+	}
+	m_size = size;
+	m_data = malloc_device<T>(size, q);
 }
 
 /**
@@ -34,11 +34,11 @@ DeviceMem<T>::DeviceMem(int size, queue q) {
  */
 template<typename T>
 void DeviceMem<T>::allocate(int size, queue q) {
-    if (m_size != 0 || size <= 0) {
-        return;
-    }
-    m_size = size;
-    m_data = malloc_device<T>(size, q);
+	if (m_size != 0 || size <= 0) {
+		return;
+	}
+	m_size = size;
+	m_data = malloc_device<T>(size, q);
 }
 
 /**
@@ -48,8 +48,8 @@ void DeviceMem<T>::allocate(int size, queue q) {
  */
 template<typename T>
 void DeviceMem<T>::free_mem(queue q) {
-    m_size = 0;
-    free(m_data, q);
+	m_size = 0;
+	free(m_data, q);
 }
 
 /**
@@ -58,8 +58,8 @@ void DeviceMem<T>::free_mem(queue q) {
  */
 template<typename T>
 void DeviceMem<T>::free_mem() {
-    m_size = 0;
-    free(m_data);
+	m_size = 0;
+	free(m_data);
 }
 
 /**
@@ -71,8 +71,8 @@ void DeviceMem<T>::free_mem() {
  */
 template<typename T>
 void DeviceMem<T>::copy_from_host(std::vector<T>& data, int n, queue q) {
-    q.memcpy(m_data, data.data(), n * sizeof(T));
-    q.wait();
+	q.memcpy(m_data, data.data(), n * sizeof(T));
+	q.wait();
 }
 
 /**
@@ -84,8 +84,8 @@ void DeviceMem<T>::copy_from_host(std::vector<T>& data, int n, queue q) {
  */
 template<typename T>
 void DeviceMem<T>::copy_to_host(std::vector<T>& data, int n, queue q) {
-    q.memcpy(data.data(), m_data, n * sizeof(T));
-    q.wait();
+	q.memcpy(data.data(), m_data, n * sizeof(T));
+	q.wait();
 }
 
 /**
@@ -96,7 +96,7 @@ void DeviceMem<T>::copy_to_host(std::vector<T>& data, int n, queue q) {
  */
 template<typename T>
 void DeviceMem<T>::copy_from_host(std::vector<T>& data, queue q) {
-    copy_from_host(data, m_size, q);
+	copy_from_host(data, m_size, q);
 }
 
 /**
@@ -108,7 +108,7 @@ void DeviceMem<T>::copy_from_host(std::vector<T>& data, queue q) {
  */
 template<typename T>
 void DeviceMem<T>::copy_to_host(std::vector<T>& data, queue q) {
-    copy_to_host(data, m_size, q);
+	copy_to_host(data, m_size, q);
 }
 
 /**
@@ -119,7 +119,7 @@ void DeviceMem<T>::copy_to_host(std::vector<T>& data, queue q) {
  */
 template<typename T>
 void DeviceMem<T>::set_data(int id, T value) {
-    m_data[id] = value;
+	m_data[id] = value;
 }
 
 /**
@@ -139,45 +139,45 @@ void DeviceMem<T>::set_data(int id, T value) {
  */
 template<typename T>
 void DeviceMem<T>::initialize_normal(double dev, DeviceMem<T>& transposed, int input_width, int width, int output_width, int n_hidden, queue q) {
-    auto p = m_data;
-    std::default_random_engine gen;
-    std::normal_distribution<double> distrib(0.0, dev);
-    T rnd;
-    std::vector<T> data(m_size);
-    std::vector<T> dataT(m_size);
-    for (int i = 0; i < input_width; i++) {
-        for (int j = 0; j < width; j++) {
-            rnd = (T)distrib(gen);
-            data[toPackedLayoutCoord(i * width + j, input_width, width)] = rnd;
-            dataT[toPackedLayoutCoord(j * width + i, width, input_width)] = rnd;
-        }
-    }
-    for (int k = 0; k < n_hidden; k++) {
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < width; j++) {
-                rnd = (T)distrib(gen);
-                data[input_width * width + k * width * width + toPackedLayoutCoord(i * width + j, width, width)] = rnd;
-                dataT[input_width * width + k * width * width + toPackedLayoutCoord(j * width + i, width, width)] = rnd;
-            }
-        }
-    }
-    for (int i = 0; i < width; i++) {
-        for (int j = 0; j < output_width; j++) {
-            rnd = (T)distrib(gen);
-            data[input_width * width + n_hidden * width * width + toPackedLayoutCoord(i * output_width + j, width, output_width)] = rnd;
-            dataT[input_width * width + n_hidden * width * width + toPackedLayoutCoord(j * output_width + i, output_width, width)] = rnd;
-        }
-    }
-    buffer<T, 1> buf(data.data(), data.size());
-    buffer<T, 1> bufT(dataT.data(), dataT.size());
-    q.submit([&](handler& h) {
-        auto acc = buf.get_access(h);
-        auto accT = bufT.get_access(h);
-        h.parallel_for(m_size, [=](id<1> idx) {
-            p[idx] = acc[idx];
-            transposed.data()[idx] = accT[idx];
-            });
-        });
+	auto p = m_data;
+	std::default_random_engine gen;
+	std::normal_distribution<double> distrib(0.0, dev);
+	T rnd;
+	std::vector<T> data(m_size);
+	std::vector<T> dataT(m_size);
+	for (int i = 0; i < input_width; i++) {
+		for (int j = 0; j < width; j++) {
+			rnd = (T)distrib(gen);
+			data[toPackedLayoutCoord(i * width + j, input_width, width)] = rnd;
+			dataT[toPackedLayoutCoord(j * width + i, width, input_width)] = rnd;
+		}
+	}
+	for (int k = 0; k < n_hidden; k++) {
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < width; j++) {
+				rnd = (T)distrib(gen);
+				data[input_width * width + k * width * width + toPackedLayoutCoord(i * width + j, width, width)] = rnd;
+				dataT[input_width * width + k * width * width + toPackedLayoutCoord(j * width + i, width, width)] = rnd;
+			}
+		}
+	}
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < output_width; j++) {
+			rnd = (T)distrib(gen);
+			data[input_width * width + n_hidden * width * width + toPackedLayoutCoord(i * output_width + j, width, output_width)] = rnd;
+			dataT[input_width * width + n_hidden * width * width + toPackedLayoutCoord(j * output_width + i, output_width, width)] = rnd;
+		}
+	}
+	buffer<T, 1> buf(data.data(), data.size());
+	buffer<T, 1> bufT(dataT.data(), dataT.size());
+	q.submit([&](handler& h) {
+		auto acc = buf.get_access(h);
+		auto accT = bufT.get_access(h);
+		h.parallel_for(m_size, [=](id<1> idx) {
+			p[idx] = acc[idx];
+			transposed.data()[idx] = accT[idx];
+			});
+		});
 }
 
 /**
@@ -191,13 +191,13 @@ void DeviceMem<T>::initialize_normal(double dev, DeviceMem<T>& transposed, int i
  */
 template<typename T>
 void DeviceMem<T>::initialize_normal(double dev, queue q) {
-    std::default_random_engine gen;
-    std::normal_distribution<double> distrib(0.0, dev);
-    std::vector<T> data(m_size);
-    for (int i = 0; i < m_size; i++) {
-        data[i] = (T)distrib(gen);
-    }
-    q.memcpy(m_data, data.data(), m_size * sizeof(T));
+	std::default_random_engine gen;
+	std::normal_distribution<double> distrib(0.0, dev);
+	std::vector<T> data(m_size);
+	for (int i = 0; i < m_size; i++) {
+		data[i] = (T)distrib(gen);
+	}
+	q.memcpy(m_data, data.data(), m_size * sizeof(T));
 }
 
 /**
@@ -216,50 +216,50 @@ void DeviceMem<T>::initialize_normal(double dev, queue q) {
  */
 template<typename T>
 void DeviceMem<T>::initialize_uniform(double scale, DeviceMem<T>& transposed, int input_width, int width, int output_width, int n_hidden, queue q) {
-    auto p = m_data;
-    std::default_random_engine gen;
-    std::uniform_real_distribution<double> distrib(0.0, scale);
+	auto p = m_data;
+	std::default_random_engine gen;
+	std::uniform_real_distribution<double> distrib(0.0, scale);
 
-    T rnd;
+	T rnd;
 
-    std::vector<T> data(m_size);
-    std::vector<T> dataT(m_size);
+	std::vector<T> data(m_size);
+	std::vector<T> dataT(m_size);
 
-    for (int i = 0; i < input_width; i++) {
-        for (int j = 0; j < width; j++) {
-            rnd = (T)distrib(gen);
-            data[toPackedLayoutCoord(i * width + j, input_width, width)] = rnd;
-            dataT[toPackedLayoutCoord(j * width + i, width, input_width)] = rnd;
-        }
-    }
+	for (int i = 0; i < input_width; i++) {
+		for (int j = 0; j < width; j++) {
+			rnd = (T)distrib(gen);
+			data[toPackedLayoutCoord(i * width + j, input_width, width)] = rnd;
+			dataT[toPackedLayoutCoord(j * width + i, width, input_width)] = rnd;
+		}
+	}
 
-    for (int k = 0; k < n_hidden; k++) {
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < width; j++) {
-                rnd = (T)distrib(gen);
-                data[input_width * width + k * width * width + toPackedLayoutCoord(i * width + j, width, width)] = rnd;
-                dataT[input_width * width + k * width * width + toPackedLayoutCoord(j * width + i, width, width)] = rnd;
-            }
-        }
-    }
+	for (int k = 0; k < n_hidden; k++) {
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < width; j++) {
+				rnd = (T)distrib(gen);
+				data[input_width * width + k * width * width + toPackedLayoutCoord(i * width + j, width, width)] = rnd;
+				dataT[input_width * width + k * width * width + toPackedLayoutCoord(j * width + i, width, width)] = rnd;
+			}
+		}
+	}
 
-    for (int i = 0; i < width; i++) {
-        for (int j = 0; j < output_width; j++) {
-            rnd = (T)distrib(gen);
-            data[input_width * width + n_hidden * width * width + toPackedLayoutCoord(i * output_width + j, width, output_width)] = rnd;
-            dataT[input_width * width + n_hidden * width * width + toPackedLayoutCoord(j * width + i, output_width, width)] = rnd;
-        }
-    }
-    buffer<T, 1> buf(data.data(), data.size());
-    buffer<T, 1> bufT(dataT.data(), dataT.size());
-    q.submit([&](handler& h) {
-        auto acc = buf.get_access(h);
-        auto accT = bufT.get_access(h);
-        h.parallel_for(m_size, [=](id<1> idx) {
-            p[idx] = acc[idx];
-            transposed.data()[idx] = accT[idx];
-            });
-        });
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < output_width; j++) {
+			rnd = (T)distrib(gen);
+			data[input_width * width + n_hidden * width * width + toPackedLayoutCoord(i * output_width + j, width, output_width)] = rnd;
+			dataT[input_width * width + n_hidden * width * width + toPackedLayoutCoord(j * width + i, output_width, width)] = rnd;
+		}
+	}
+	buffer<T, 1> buf(data.data(), data.size());
+	buffer<T, 1> bufT(dataT.data(), dataT.size());
+	q.submit([&](handler& h) {
+		auto acc = buf.get_access(h);
+		auto accT = bufT.get_access(h);
+		h.parallel_for(m_size, [=](id<1> idx) {
+			p[idx] = acc[idx];
+			transposed.data()[idx] = accT[idx];
+			});
+		});
 }
 
 /**
@@ -278,36 +278,36 @@ void DeviceMem<T>::initialize_uniform(double scale, DeviceMem<T>& transposed, in
  */
 template<typename T>
 void DeviceMem<T>::make_transposed(DeviceMem<T>& transposed, int input_width, int width, int output_width, int n_hidden, queue q) {
-    auto p = m_data;
+	auto p = m_data;
 
-    q.parallel_for<>(range<1>(input_width * width + n_hidden * width * width + width * output_width), [=](id<1> idx) {
-        int i = 0;
-        int j = 0;
-        int mat_num = 0;
-        int mat_offset = 0;
+	q.parallel_for<>(range<1>(input_width * width + n_hidden * width * width + width * output_width), [=](id<1> idx) {
+		int i = 0;
+		int j = 0;
+		int mat_num = 0;
+		int mat_offset = 0;
 
-        if (idx < input_width * width) {
-            i = idx / input_width;
-            j = idx % input_width;
-            transposed.data()[toPackedLayoutCoord(j * width + i, width, input_width)] = p[toPackedLayoutCoord(i * width + j, input_width, width)];
-        }
+		if (idx < input_width * width) {
+			i = idx / input_width;
+			j = idx % input_width;
+			transposed.data()[toPackedLayoutCoord(j * width + i, width, input_width)] = p[toPackedLayoutCoord(i * width + j, input_width, width)];
+		}
 
-        else if (idx < input_width * width + n_hidden * width * width) {
-            mat_num = idx / (width * width);
-            mat_offset = (idx - input_width * width) % (width * width);
-            i = mat_offset / input_width;
-            j = mat_offset % input_width;
-            transposed.data()[input_width * width + mat_num * width * width + toPackedLayoutCoord(j * width + i, width, width)] = p[input_width * width + mat_num * width * width + toPackedLayoutCoord(i * width + j, width, width)];
-        }
+		else if (idx < input_width * width + n_hidden * width * width) {
+			mat_num = idx / (width * width);
+			mat_offset = (idx - input_width * width) % (width * width);
+			i = mat_offset / input_width;
+			j = mat_offset % input_width;
+			transposed.data()[input_width * width + mat_num * width * width + toPackedLayoutCoord(j * width + i, width, width)] = p[input_width * width + mat_num * width * width + toPackedLayoutCoord(i * width + j, width, width)];
+		}
 
-        else {
-            mat_offset = (idx - input_width * width - n_hidden * width * width) % (width * output_width);
-            i = mat_offset / input_width;
-            j = mat_offset % input_width;
-            transposed.data()[input_width * width + n_hidden * width * width + toPackedLayoutCoord(j * width + i, output_width, width)] = p[input_width * width + n_hidden * width * width + toPackedLayoutCoord(i * width + j, width, output_width)];
+		else {
+			mat_offset = (idx - input_width * width - n_hidden * width * width) % (width * output_width);
+			i = mat_offset / input_width;
+			j = mat_offset % input_width;
+			transposed.data()[input_width * width + n_hidden * width * width + toPackedLayoutCoord(j * width + i, output_width, width)] = p[input_width * width + n_hidden * width * width + toPackedLayoutCoord(i * width + j, width, output_width)];
 
-        }
-        });
+		}
+		});
 }
 
 /**
@@ -321,13 +321,13 @@ void DeviceMem<T>::make_transposed(DeviceMem<T>& transposed, int input_width, in
  */
 template<typename T>
 void DeviceMem<T>::initialize_uniform(queue q, double scale) {
-    std::default_random_engine gen;
-    std::uniform_real_distribution<double> distrib(0.0, scale);
-    std::vector<T> data(m_size);
-    for (int i = 0; i < m_size; i++) {
-        data[i] = (T)distrib(gen);
-    }
-    q.memcpy(m_data, data.data(), m_size * sizeof(T));
+	std::default_random_engine gen;
+	std::uniform_real_distribution<double> distrib(0.0, scale);
+	std::vector<T> data(m_size);
+	for (int i = 0; i < m_size; i++) {
+		data[i] = (T)distrib(gen);
+	}
+	q.memcpy(m_data, data.data(), m_size * sizeof(T));
 }
 /**
  * Initialize the device memory with values sampled from a Xavier uniform distribution.
@@ -344,8 +344,8 @@ void DeviceMem<T>::initialize_uniform(queue q, double scale) {
  */
 template<typename T>
 void DeviceMem<T>::initialize_xavier_unif(DeviceMem<T>& transposed, int input_width, int width, int output_width, int n_hidden, queue q) {
-    double x = sqrt(6.0 / ((double)(input_width + output_width)));
-    initialize_uniform(x, transposed, input_width, width, output_width, n_hidden, q);
+	double x = sqrt(6.0 / ((double)(input_width + output_width)));
+	initialize_uniform(x, transposed, input_width, width, output_width, n_hidden, q);
 }
 /**
  * Initialize the device memory with values sampled from a Xavier uniform distribution.
@@ -359,8 +359,8 @@ void DeviceMem<T>::initialize_xavier_unif(DeviceMem<T>& transposed, int input_wi
  */
 template<typename T>
 void DeviceMem<T>::initialize_xavier_unif(int input_width, int output_width, queue q) {
-    double x = sqrt(6.0 / ((double)(input_width + output_width)));
-    initialize_uniform(q, x);
+	double x = sqrt(6.0 / ((double)(input_width + output_width)));
+	initialize_uniform(q, x);
 }
 /**
  * Initialize the device memory with values sampled from a Xavier normal distribution.
@@ -377,8 +377,8 @@ void DeviceMem<T>::initialize_xavier_unif(int input_width, int output_width, que
  */
 template<typename T>
 void DeviceMem<T>::inititialize_xavier_normal(DeviceMem<T>& transposed, int input_width, int width, int output_width, int n_hidden, queue q) {
-    double dev = sqrt(2.0 / ((double)(input_width + output_width)));
-    initialize_normal(dev, transposed, input_width, width, output_width, n_hidden, q);
+	double dev = sqrt(2.0 / ((double)(input_width + output_width)));
+	initialize_normal(dev, transposed, input_width, width, output_width, n_hidden, q);
 }
 
 /**
@@ -393,8 +393,8 @@ void DeviceMem<T>::inititialize_xavier_normal(DeviceMem<T>& transposed, int inpu
  */
 template<typename T>
 void DeviceMem<T>::initialize_xavier_normal(int input_width, int output_width, queue q) {
-    double dev = sqrt(2.0 / ((double)(input_width + output_width)));
-    initialize_normal(dev, q);
+	double dev = sqrt(2.0 / ((double)(input_width + output_width)));
+	initialize_normal(dev, q);
 }
 
 /**
@@ -409,9 +409,9 @@ void DeviceMem<T>::initialize_xavier_normal(int input_width, int output_width, q
  */
 template<typename T>
 void DeviceMem<T>::initialize_constant(T constant, DeviceMem<T>& transposed, queue q) {
-    std::vector<T> data(m_size, constant);
-    q.memcpy(m_data, data.data(), m_size * sizeof(T));
-    q.memcpy(transposed.data(), data.data(), m_size * constant);
+	std::vector<T> data(m_size, constant);
+	q.memcpy(m_data, data.data(), m_size * sizeof(T));
+	q.memcpy(transposed.data(), data.data(), m_size * constant);
 }
 
 /**
@@ -424,10 +424,10 @@ void DeviceMem<T>::initialize_constant(T constant, DeviceMem<T>& transposed, que
  */
 template<typename T>
 void DeviceMem<T>::initialize_constant(T constant, queue q) {
-    auto p = m_data;
-    q.parallel_for<>(range<1>(m_size), [=](id<1> idx) {
-        p[idx] = (T)constant;
-        }).wait();
+	auto p = m_data;
+	q.parallel_for<>(range<1>(m_size), [=](id<1> idx) {
+		p[idx] = (T)constant;
+		}).wait();
 }
 
 /**
@@ -445,8 +445,8 @@ void DeviceMem<T>::initialize_constant(T constant, queue q) {
  */
 template<typename T>
 void DeviceMem<T>::intitialize_he_normal(DeviceMem<T>& transposed, int input_width, int width, int output_width, int n_hidden, queue q) {
-    double dev = sqrt(2.0 / width);
-    initialize_normal(dev, transposed, input_width, width, output_width, n_hidden, q);
+	double dev = sqrt(2.0 / width);
+	initialize_normal(dev, transposed, input_width, width, output_width, n_hidden, q);
 }
 
 /**
@@ -460,8 +460,8 @@ void DeviceMem<T>::intitialize_he_normal(DeviceMem<T>& transposed, int input_wid
  */
 template<typename T>
 void DeviceMem<T>::intitialize_he_normal(int input_width, queue q) {
-    double dev = sqrt(2.0 / input_width);
-    initialize_normal(dev, q);
+	double dev = sqrt(2.0 / input_width);
+	initialize_normal(dev, q);
 }
 
 template class DeviceMem<float>;
