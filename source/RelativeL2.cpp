9aad2244bb390b11e95f3607f444a5357048c243
DariusDabert <115560159+DariusDabert@users.noreply.github.com>
2023-08-31 11:54:16 +0200

    Update RelativeL2.cpp

--- source/RelativeL2.cpp
+++ source/RelativeL2.cpp
@@ -40,48 +40,42 @@ void Relative_L2_loss(id<1> idx,
 	grads[idx] = bf16(scale * 2 * difference / var / N_total_elements);
 }
 
-/**
- * Class for evaluating relative L2 loss and gradients.
- */
-class RelativeL2Loss {
-public:
-	/**
-	 * Evaluate relative L2 loss and gradients.
-	 *
-	 * @param q SYCL queue for parallel computation.
-	 * @param dims Number of dimensions.
-	 * @param stride Stride value for indexing.
-	 * @param scale Scaling factor for gradients.
-	 * @param preds Predicted values (DeviceMem<float>).
-	 * @param targets Target values (DeviceMem<float>).
-	 * @param grads Gradient values (DeviceMem<bf16>).
-	 * @param values Array to store loss values (DeviceMem<float>).
-	 */
-	void evaluate(
-		queue q,
-		const int dims,
-		const int stride,
-		const float scale,
-		DeviceMem<float>& preds,
-		DeviceMem<float>& targets,
-		DeviceMem<bf16>& grads,
-		DeviceMem<float>& values
-	) {
-		// Get the total number of elements
-		int n_elements = preds.size();
 
+/**
+ * Evaluate relative L2 loss and gradients.
+ *
+ * @param q SYCL queue for parallel computation.
+ * @param dims Number of dimensions.
+ * @param stride Stride value for indexing.
+ * @param scale Scaling factor for gradients.
+ * @param preds Predicted values (DeviceMem<float>).
+ * @param targets Target values (DeviceMem<float>).
+ * @param grads Gradient values (DeviceMem<bf16>).
+ * @param values Array to store loss values (DeviceMem<float>).
+ */
+void RelativeL2Loss::evaluate(
+	queue q,
+	const int dims,
+	const int stride,
+	const float scale,
+	DeviceMem<float>& preds,
+	DeviceMem<float>& targets,
+	DeviceMem<bf16>& grads,
+	DeviceMem<float>& values
+) {
+	// Get the total number of elements
+	int n_elements = preds.size();
 		// Perform parallel computation using SYCL
-		q.parallel_for<>(range<1>(n_elements), [=](id<1> idx) {
-			// Call the Relative_L2_loss function to calculate loss and gradients
-			Relative_L2_loss(idx,
-				n_elements,
-				dims,
-				stride,
-				scale,
-				preds.data(),
-				targets.data(),
-				grads.data(),
-				values.data());
-		}).wait();
-	}
-};
+	q.parallel_for<>(range<1>(n_elements), [=](id<1> idx) {
+		// Call the Relative_L2_loss function to calculate loss and gradients
+		Relative_L2_loss(idx,
+			n_elements,
+			dims,
+			stride,
+			scale,
+			preds.data(),
+			targets.data(),
+			grads.data(),
+			values.data());
+	}).wait();
+}
