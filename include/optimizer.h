1eb096275187cbace38b9b6d8cc50ad9c0a8ddd1
DariusDabert <115560159+DariusDabert@users.noreply.github.com>
2023-08-31 11:36:39 +0200

    Update optimizer.h

--- include/optimizer.h
+++ include/optimizer.h
@@ -1,10 +1,8 @@
 #pragma once
 
 #include <stdint.h>
-#include <json/json.hpp>
 #include "DeviceMem.h"
 
-using json = nlohmann::json;
 using bf16 = sycl::ext::oneapi::bfloat16;
 
 class Optimizer {
