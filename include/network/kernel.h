7baa144fc650c6be9967842c235571e495e3257b
Christoph Bauinger <christoph.bauinger@intel.com>
2023-11-13 09:44:02 -0800

    New kernel compiles now. Todo: Clean up the helper functions more. Check if we can make this even more general to work for all WIDTHs

--- include/network/kernel.h
+++ include/network/kernel.h
@@ -15,10 +15,12 @@
 
 #pragma once
 
+#include <algorithm>
 #include <sycl/sycl.hpp>
 #include <vector>
 
 #include "common.h"
+#include "kernel_helper.h"
 
 namespace tinydpcppnn {
 namespace kernels {
@@ -34,10 +36,102 @@ using namespace sycl::ext::oneapi::experimental::matrix;
 // This is the general forward map which also doubles as inference. We use template
 // specialization for all the versions
 template <typename T, typename Tc, int INPUT_WIDTH, int OUTPUT_WIDTH, Activation activation,
-          Activation output_activation, bool INFERENCE, size_t TN>
-std::vector<sycl::event> mlp_swift_forward_4(sycl::queue &q, T const *const __restrict__ weights_ptr,
-                                             T const *const __restrict__ inputs_ptr,
-                                             T *const __restrict__ intermediate_output, const int n_hidden_layers,
-                                             const int M, const std::vector<sycl::event> &deps);
+          Activation output_activation, bool INFERENCE, std::size_t TN>
+std::vector<sycl::event>
+forward_impl_4(sycl::queue &q, T const *const __restrict__ weights_ptr, T const *const __restrict__ inputs_ptr,
+               T *const __restrict__ intermediate_output, const int n_hidden_layers, const int M,
+               const std::vector<sycl::event> &deps) { // reuse of B, this is in subgroups, ONLY works for 64
+    // note that large grf mode requires this to be set to 32, but then this code does not work anymore
+    constexpr int SG_SIZE = TN;
+    const int SGS_IN_WG =
+        q.get_device().get_info<sycl::info::device::max_work_group_size>() / SG_SIZE; // maximum number
+    constexpr int WIDTH = 4 * TN;
+    constexpr size_t TM = 8;                                             // this may be adjusted in the future
+    constexpr size_t TK = 8 * std::min<size_t>(8, 32 / (8 * sizeof(T))); // This depends on the datatype T
+    assert(M % TM == 0); // make sure there is no remainder and no out of bounds accesses
+    static_assert(INPUT_WIDTH == WIDTH);
+    static_assert(OUTPUT_WIDTH == WIDTH);
+
+    // One Block Row has TM rows an N columns.
+    auto e = q.submit([&](sycl::handler &cgh) {
+        cgh.depends_on(deps);
+
+        sycl::local_accessor<T, 1> B(sycl::range<1>(WIDTH * WIDTH), cgh);
+        sycl::local_accessor<T, 1> Atmp(sycl::range<1>(TM * WIDTH * SGS_IN_WG), cgh);
+
+        // number of SGS is given by M / TM, since batch_size is the number of rows in the output
+        cgh.parallel_for(
+            sycl::nd_range<1>(M / TM * SG_SIZE, SGS_IN_WG * SG_SIZE),
+            [=](sycl::nd_item<1> item) [[intel::reqd_sub_group_size(SG_SIZE)]] {
+                auto sg = item.get_sub_group();
+
+                T const *weights_ptr_loc = weights_ptr;
+                auto A_sg_start = local_ptr<T>(&Atmp[sg.get_group_id()[0] * WIDTH * TM]);
+                auto B_ptr = local_ptr<T>(&B[0]);
+
+                // offset in all the data
+                const int wg_and_sg_offset_A =
+                    item.get_group().get_group_id() * SGS_IN_WG * WIDTH * TM + sg.get_group_id()[0] * WIDTH * TM;
+                int layer_offset_A = M * WIDTH + wg_and_sg_offset_A;
+
+                helpers::moveMemory<WIDTH, WIDTH>(item, global_ptr<const T>(weights_ptr_loc), B_ptr);
+                weights_ptr_loc += WIDTH * WIDTH; // next weight matrix
+
+                // load input in slm
+                helpers::moveMemorySG<TM, WIDTH>(sg, global_ptr<const T>(inputs_ptr + wg_and_sg_offset_A), A_sg_start);
+
+                // if not inference activate and store in intermediate output
+                if constexpr (!INFERENCE)
+                    helpers::applyActivation(sg, A_sg_start, global_ptr<T>(intermediate_output + wg_and_sg_offset_A));
+
+                joint_matrix<sycl::sub_group, Tc, use::accumulator, TM, TN> C_block0, C_block1, C_block2,
+                    C_block3; // this is the only reason why this is not general
+                for (int layer = 0; layer < n_hidden_layers; layer++) {
+                    // reset result matrices
+                    helpers::zeroMatrices(sg, C_block0, C_block1, C_block2, C_block3);
+
+                    // ensure weight matrix is loaded
+                    item.barrier(sycl::access::fence_space::local_space);
+
+                    helpers::MAD<WIDTH, TK>(sg, A_sg_start, B_ptr, C_block0, C_block1, C_block2, C_block3);
+
+                    item.barrier(sycl::access::fence_space::local_space);
+                    // load next weight matrix
+                    helpers::moveMemory<WIDTH, WIDTH>(item, global_ptr<const T>(weights_ptr_loc), B_ptr);
+
+                    // activate and save
+                    helpers::applyActivation<activation, WIDTH>(sg, C_block0, A_sg_start);
+                    helpers::applyActivation<activation, WIDTH>(sg, C_block1, A_sg_start + TN);
+                    helpers::applyActivation<activation, WIDTH>(sg, C_block2, A_sg_start + 2 * TN);
+                    helpers::applyActivation<activation, WIDTH>(sg, C_block3, A_sg_start + 3 * TN);
+
+                    if constexpr (!INFERENCE)
+                        helpers::moveMemorySG<TM, WIDTH>(sg, A_sg_start,
+                                                         global_ptr<T>(intermediate_output + layer_offset_A));
+
+                    layer_offset_A += M * WIDTH;
+                }
+
+                // generate output, i.e. last GEMM
+                helpers::zeroMatrices(sg, C_block0, C_block1, C_block2, C_block3);
+
+                item.barrier(sycl::access::fence_space::local_space); // wait for B to be loaded
+
+                helpers::MAD<WIDTH, TK>(sg, A_sg_start, B_ptr, C_block0, C_block1, C_block2, C_block3);
+
+                // activate and save to slm
+                helpers::applyActivation<output_activation, WIDTH>(sg, C_block0, A_sg_start);
+                helpers::applyActivation<output_activation, WIDTH>(sg, C_block1, A_sg_start + TN);
+                helpers::applyActivation<output_activation, WIDTH>(sg, C_block2, A_sg_start + 2 * TN);
+                helpers::applyActivation<output_activation, WIDTH>(sg, C_block3, A_sg_start + 3 * TN);
+
+                // save slm to HBM
+                helpers::moveMemorySG<TM, WIDTH>(sg, A_sg_start, global_ptr<T>(intermediate_output + layer_offset_A));
+            });
+    });
+
+    return {e};
+}
+
 } // namespace kernels
 } // namespace tinydpcppnn
---
+++ include/network/kernel_helper.h
@@ -0,0 +1,149 @@
+// Copyright (C) 2023 Intel Corporation
+// SPDX-License-Identifier: BSD-3-Clause
+
+// header file which implements functions commonly used in all the kernels, like
+// loading, storing + specializations for different types
+// multiplications patterns, etc.
+
+#pragma once
+
+#include <sycl/sycl.hpp>
+#include <vector>
+
+#include "common.h"
+
+namespace tinydpcppnn {
+namespace kernels {
+namespace helpers {
+
+using namespace sycl::ext::oneapi::experimental::matrix;
+using sycl::global_ptr;
+using sycl::local_ptr;
+
+// load a submatrix row-major piece of size MxN int SLM
+template <int M, int N, typename Tsrc, typename Tdest, sycl::access::address_space AddressSpacesrc,
+          sycl::access::decorated IsDecoratedsrc, sycl::access::address_space AddressSpacedest,
+          sycl::access::decorated IsDecorateddest>
+static inline void moveMemory(sycl::nd_item<1> &item, const sycl::multi_ptr<Tsrc, AddressSpacesrc, IsDecoratedsrc> &src,
+                              sycl::multi_ptr<Tdest, AddressSpacedest, IsDecorateddest> dest) {
+
+    for (int iter = item.get_local_linear_id(); iter < M * N; iter += item.get_local_range(0)) {
+        dest[iter] = static_cast<Tdest>(src[iter]);
+    }
+}
+
+// load a submatrix row-major piece of size MxN int SLM, sub-group by sub-group
+template <int M, int N, typename Tsrc, typename Tdest, typename Group, sycl::access::address_space AddressSpacesrc,
+          sycl::access::decorated IsDecoratedsrc, sycl::access::address_space AddressSpacedest,
+          sycl::access::decorated IsDecorateddest>
+static inline void moveMemorySG(Group sg, const sycl::multi_ptr<Tsrc, AddressSpacesrc, IsDecoratedsrc> &src,
+                                sycl::multi_ptr<Tdest, AddressSpacedest, IsDecorateddest> dest) {
+
+    for (int iter = sg.get_local_id()[0]; iter < M * N; iter += sg.get_local_range()[0]) {
+        dest[iter] = static_cast<Tdest>(src[iter]);
+    }
+}
+
+template <typename Group, typename T, use Use, size_t NumRows, size_t NumCols, layout Layout>
+static inline void zeroMatrices(Group sg, joint_matrix<Group, T, Use, NumRows, NumCols, Layout> &matrix) {
+    joint_matrix_fill(sg, matrix, static_cast<T>(0));
+}
+
+template <typename Group, typename T, use Use, size_t NumRows, size_t NumCols, layout Layout, typename... Args>
+static inline void zeroMatrices(Group sg, joint_matrix<Group, T, Use, NumRows, NumCols, Layout> &matrix, Args... args) {
+    joint_matrix_fill(sg, matrix, static_cast<T>(0));
+    zeroMatrices(sg, args...);
+}
+
+template <int WIDTH, std::size_t K, typename Group, typename Ta, typename Tb, typename Tc, std::size_t M, std::size_t N,
+          typename... AllC>
+static inline void MAD_1_ROW(
+    Group sg, joint_matrix<Group, Ta, use::a, M, K, layout::row_major> &mA, const local_ptr<Tb> &B,
+    joint_matrix<Group, Tc, use::accumulator, M, N, sycl::ext::oneapi::experimental::matrix::layout::dynamic> &mC) {
+
+    constexpr int vnni_factor = std::max<int>(4 / sizeof(Tb), 1); // how many rows are consecutive elements
+    joint_matrix<Group, Tb, use::b, K, N, sycl::ext::intel::experimental::matrix::layout::packed> mB;
+    joint_matrix_load(
+        sg, mB, B, vnni_factor * WIDTH); // B is vnnied, which only has on impact if Tb is a datatype sized less than 4
+    mC = joint_matrix_mad(sg, mA, mB, mC);
+}
+
+template <int WIDTH, std::size_t K, typename Group, typename Ta, typename Tb, typename Tc, std::size_t M, std::size_t N,
+          typename... AllC>
+static inline void
+MAD_1_ROW(Group sg, joint_matrix<Group, Ta, use::a, M, K, layout::row_major> &mA, const local_ptr<Tb> &B,
+          joint_matrix<Group, Tc, use::accumulator, M, N, sycl::ext::oneapi::experimental::matrix::layout::dynamic> &mC,
+          AllC &...Cs) {
+
+    constexpr int vnni_factor = std::max<int>(4 / sizeof(Tb), 1); // how many rows are consecutive elements
+    joint_matrix<Group, Tb, use::b, K, N, sycl::ext::intel::experimental::matrix::layout::packed> mB;
+    // B is vnnied, which only has on impact if Tb is a datatype sized less than 4
+    joint_matrix_load(sg, mB, B, vnni_factor * WIDTH);
+    mC = joint_matrix_mad(sg, mA, mB, mC);
+    // increment B to get next block col and call recursively on remaining C's
+    MAD_1_ROW<WIDTH, K>(sg, mA, B + vnni_factor * N, Cs...);
+}
+
+template <int WIDTH, std::size_t K, typename Group, typename Ta, typename Tb, typename Tc, std::size_t M, std::size_t N,
+          typename... AllC>
+static inline void
+MAD_1_ROW(Group sg, const local_ptr<Ta> &A, const local_ptr<Tb> &B,
+          joint_matrix<Group, Tc, use::accumulator, M, N, sycl::ext::oneapi::experimental::matrix::layout::dynamic> &mC,
+          AllC &...Cs) {
+
+    // First load the matrix A from slm, then call the jount matrix based version above
+    joint_matrix<Group, Ta, use::a, M, K, layout::row_major> mA;
+    // A is not vnnied
+    joint_matrix_load(sg, mA, A, WIDTH);
+    MAD_1_ROW<WIDTH, K>(sg, mA, B, mC,
+                        Cs...); // increment B to get next block col and call recursively on remaining C's
+}
+
+template <int WIDTH, std::size_t K, typename Group, typename Ta, typename Tb, typename Tc, std::size_t M, std::size_t N,
+          typename... AllC>
+static inline void
+MAD(Group sg, const local_ptr<Ta> &A, const local_ptr<Tb> &B,
+    joint_matrix<Group, Tc, use::accumulator, M, N, sycl::ext::oneapi::experimental::matrix::layout::dynamic> &mC,
+    AllC &...Cs) {
+
+    for (int aiter = 0; aiter < WIDTH; aiter += K) {
+        MAD_1_ROW<WIDTH, K>(sg, A + aiter, B + aiter * WIDTH, mC, Cs...);
+    }
+}
+
+template <typename Tin, typename Tout, Activation act> inline void activate(const Tin &data_in, Tout &data_out) {
+    if constexpr (act == Activation::None)
+        data_out = static_cast<Tout>(data_in);
+    else if constexpr (act == Activation::ReLU)
+        data_out = static_cast<Tout>(std::max<Tin>(static_cast<Tin>(0), data_in));
+}
+
+template <Activation act, int WIDTH, typename Group, use Use, typename Tout, typename Tin, std::size_t NumRows,
+          std::size_t NumCols, layout Layout, sycl::access::address_space AddressSpace,
+          sycl::access::decorated IsDecorated>
+static inline void applyActivation(Group sg, joint_matrix<Group, Tin, Use, NumRows, NumCols, Layout> &in,
+                                   sycl::multi_ptr<Tout, AddressSpace, IsDecorated> dest) {
+
+    auto data_in = sycl::ext::intel::experimental::matrix::get_wi_data(sg, in);
+    for (int rowiter = 0; rowiter < data_in.length(); rowiter++) // should be TM in length
+    {
+        activate<Tin, Tout, act>(static_cast<Tin>(data_in[rowiter]), dest[rowiter * WIDTH + sg.get_local_id()[0]]);
+    }
+}
+
+// row stride >= NumCols!
+template <Activation act, int M, int N, typename Group, typename Tout, typename Tin,
+          sycl::access::address_space AddressSpacesrc, sycl::access::decorated IsDecoratedsrc,
+          sycl::access::address_space AddressSpacedest, sycl::access::decorated IsDecorateddest>
+static inline void applyActivation(Group sg, const sycl::multi_ptr<Tin, AddressSpacesrc, IsDecoratedsrc> &src,
+                                   sycl::multi_ptr<Tout, AddressSpacedest, IsDecorateddest> dest) {
+
+    for (int iter = sg.get_local_id()[0]; iter < M * N; iter += sg.get_local_range()[0]) // should be TM in length
+    {
+        activate<Tin, Tout, act>(static_cast<Tin>(src[iter]), dest[iter]);
+    }
+}
+
+} // namespace helpers
+} // namespace kernels
+} // namespace tinydpcppnn
